989d04fff37ede677c7cc7b4aa4f52310d471af8
Mia McCullough <143734366+miamccu@users.noreply.github.com>
2024-04-13 14:06:00 -0500

    these are temp files. they only have the CSS for the navi bar.
    These files don't have any of the needed code to perform their respective functions. I only have them so that I could check if the navi bars in the inventory.php and view_orders.php files worked and went to these pages.

---
+++ fulfill_orders.php
@@ -0,0 +1,142 @@
+<html>
+ <head>
+  <title>Fulfill Orders</title>
+  <style>
+
+   table
+   {
+     border-collapse: collapse;
+     width: 100%;
+   }
+
+   th, td
+   {
+     padding: 8px;
+     text-align: left;
+     border-bottom: 1px solid #ddd;
+   }
+
+   tr:nth-child(even)
+   {
+     background-color: #f2f2f2;
+   }
+
+   body
+   {
+     font-family: Arial, sans-serif;
+     margin: 0;
+     padding: 0;
+     background-color: #fff;
+   }
+
+   header
+   {
+     background-color:#848B79;
+     color: #000000;
+     text-align: center;
+     padding: 1em 0;
+   }
+
+   header a
+   {
+     color: #fff;
+     text-decoration: none;
+     margin: 0 15px;
+   }
+
+   .navbar
+   {
+     background-color:#848B79;
+     overflow: hidden;
+   }
+
+   .navbar a
+   {
+     float: left;
+     display: block;
+     color: white;
+     text-align: center;
+     padding: 14px 16px;
+     text-decoration: none;
+   }
+
+   .navbar a:hover
+   {
+     background-color: #848B79;
+     color: black;
+   }
+
+   .navbar .icon
+   {
+     display: none;
+   }
+
+   @media screen and (max-width: 600px)
+   {
+     .navbar a:not(:first-child)
+     {
+       display: none;
+     }
+
+     .navbar a.icon
+     {
+       float: right;
+       display: block;
+     }
+   }
+
+   @media screen and (max-width: 600px)
+   {
+     .navbar.responsive
+     {
+       position: relative;
+     }
+
+     .navbar.responsive .icon
+     {
+       position: absolute;
+       right: 0;
+       top: 0;
+     }
+
+     .navbar.responsive a
+     {
+       float: none;
+       display: block;
+       text-align: left;
+     }
+   }
+
+  </style>
+ </head>
+  <body>
+   <header>
+     <div class="navbar" id="navbar">
+      <a href='fulfill_orders.php'>Fulfill Orders</a>
+      <a href='inventory.php'>Update Inventory</a>
+      <a href="javascript:void(0);" class="icon" onclick="myFunction()"> &#9776; </a>
+     </div>
+   </header>
+
+   <?php
+
+    include("secrets.php"); //this is another php, that has a $username, $password, and $dbname to connect to the db
+                                                            //zid,       bday,          zid
+
+    try{
+      $dsn1 = "mysql:host=blitz.cs.niu.edu;dbname=csci467";
+      $dsn2 = "mysql:host=courses;dbname=$dbname";
+      $pdo1 = new PDO($dsn1, "student", "student");
+      $pdo2 = new PDO($dsn2, $username, $password);
+
+      echo "this is a temp fulfill_orders.php";
+
+      //code here
+
+    }
+    catch(PDOexception $e) {
+      echo "Connection to database failed: " . $e->getMessage();
+    }
+   ?>
+  </body>
+</html>
